c2b711a52d5db1f8329b50013c0b236bfaa9a6af
Rodolfo Mori <rod_dolpho@hotmail.com>
2020-09-24 17:02:19 -0300

    add styles

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-import { Container, MainData, Wrapper, InputSearchData, Span } from './styles';
+import { Container, MainData, Wrapper, InputSearchData, Span, Calculate, Column, Observations } from './styles';
 import { penalCode } from '../../penal-code'
 
 function Main() {
@@ -14,14 +14,17 @@ function Main() {
 
     const calculate = () => {
         console.log(crimesCommitted)
+        if (crimesCommitted.length > 0) {
+            const filtered = penalCodeData.filter(data => crimesCommitted.includes(data.id))
 
-        const filtered = penalCodeData.filter(data => crimesCommitted.includes(data.id))
+            const sentence = filtered.map(data => data.pena).reduce((acc, curr) => acc + curr)
 
-        const sentence = filtered.map(data => data.pena).reduce((acc, curr) => acc + curr)
+            const penalty = filtered.map(data => data.multa).reduce((acc, curr) => acc + curr)
 
-        const penalty = filtered.map(data => data.multa).reduce((acc, curr) => acc + curr)
-
-        alert(`Sua sentença é de ${sentence} meses, ou ${sentence / 5} minutos. E deve pagar uma multa de ${(penalty).toLocaleString('pt-BR')}`)
+            alert(`Sua sentença é de ${sentence} meses, ou ${sentence / 5} minutos. E deve pagar uma multa de $ ${(penalty).toLocaleString('pt-BR')}`)
+        } else {
+            alert(`Escolha pelo menos um crime!`)
+        }
     }
 
     const setField = (id) => {
@@ -36,38 +39,58 @@ function Main() {
 
     }
     return (
-        <Container>
-            <InputSearchData
-                value={inputSearchData}
-                onChange={
-                    (value) => {
-                        setInputSearchData(value.target.value)
+        <div style={{
+            display: "flex",
+            justifyContent: "center"
+        }}>
+            <Container>
+                <InputSearchData
+                    placeholder="Pesquise por nome da inflação"
+                    value={inputSearchData}
+                    onChange={
+                        (value) => {
+                            setInputSearchData(value.target.value)
+                        }
                     }
-                }
-            />
-            {penalCodeData && (
-                <MainData>
-                    {penalCodeData.map(data => (
-                        data.crime.toLowerCase().includes(inputSearchData.toLowerCase()) && (
-                            <Wrapper key={data.id} show={data.crime.includes(inputSearchData)}>
-                                <input type="checkbox" onChange={() => setField(data.id)} />
-                                <div>
-                                    <div><Span>Crime:</Span> {data.crime}</div>
-                                    <div><Span>Reclusão:</Span> {data.pena} meses </div>
-                                    <div><Span>Observações: </Span>{data.observação}</div>
-                                    <div><Span>Categoria:</Span> {data.categoria}</div>
-                                    <div><Span>Multa: </Span>$ {(data.multa).toLocaleString('pt-BR')}</div>
+                />
+                {penalCodeData && (
 
-                                </div>
+                    <MainData>
+                        <h1 style={{textAlign: "center"}}>Código Penal</h1>
 
-                            </Wrapper>
-                        )
-                    ))}
-                </MainData>
-            )}
-            <button onClick={calculate}>Calcular</button>
-        </Container>
+                        {penalCodeData.map(data => (
+                            data.crime.toLowerCase().includes(inputSearchData.toLowerCase()) && (
+                                <Wrapper key={data.id} show={data.crime.includes(inputSearchData)}>
+                                    <input type="checkbox" onChange={() => setField(data.id)} />
+                                    <div>
+                                        <Column><Span>Crime:</Span> {data.crime}</Column>
+                                        <Column><Span>Reclusão:</Span> {data.pena} meses </Column>
+                                        <Column><Span>Observações: </Span>{data.observação}</Column>
+                                        <Column><Span>Categoria:</Span> {data.categoria === 'assaltos' ? 'Assaltos'
+                                            : data.categoria === 'nivel_1' ? 'Nível 1' : data.categoria === 'nivel_2' ? 'Nível 2' : 'Nivel 3'}</Column>
+                                        <Column><Span>Multa: </Span>$ {(data.multa).toLocaleString('pt-BR')}</Column>
 
+                                    </div>
+
+                                </Wrapper>
+                            )
+                        ))}
+                    </MainData>
+
+                )}
+                <Observations>
+                    <h2 style={{ textAlign: "center" }}>Observações</h2>
+                    <p><strong>1</strong> - A posse legal de drogas é até 6 únidades, passando da quantidade legal o mesmo é indiciado pelo crime de Tráfico de Drogas.			</p>
+                    <p><strong>2</strong> - Veículos são apreendidos quando o mesmo é utilizado em ações onde o condutor ou passageiro atenta contra a vida de qualquer pessoa, use para fugas, assaltos e etc.			</p>
+                    <p><strong>3</strong> - Quando um servidor público estiver sendo investigado o mesmo deve ser afastado imediatamente até o termino da investigação.			</p>
+                    <p><strong>4</strong> - Não há crime de cumplicidade, as pessoas que colaboram diretamente ou indiretamente para o crime receberão a pena calculada entre a metade e o total da pena.			</p>
+                    <p><strong>5</strong> - Conversas entre suspeitos e advogados são confidenciais, dê uma distância respeitosa para que os mesmos conversem sem interrupções.			</p>
+                    <p><strong>6</strong> - Negociação com advogado = Redução de até 10% da pena total.			</p>
+                    <p><strong>7</strong> - Confessar crime = Redução de até 10% da pena total.			</p>
+                </Observations>
+                <Calculate onClick={calculate}>Calcular</Calculate>
+            </Container>
+        </div>
     )
 }
 
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,27 +1,95 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid black;
+    border-radius: 10px;    max-width: 500px;
+    font-family: sans-serif;
+    background: #eee;
 `;
 
 export const MainData = styled.div`
+    margin-top: 100px;
 `;
 
 export const Wrapper = styled.div`
+    font-family: sans-serif;
     display: flex;
     align-items: center;
-    gap: 20px;
+    gap: 20px; 
     padding: 20px;
-    margin-left:40px;
     display:${props => !props.show && 'none'}
     span{
         font-weight:bold;
     }
+    input{
+        width: 17px;
+        height: 17px;
+    }
+    border-radius: 20px;
+    padding: 10px;
+    margin: 20px 0;
+    margin-left:40px;
+
+
 `;
 
+export const Column = styled.div`
+padding: 2px;
+`
+export const Observations = styled.div`
+margin: 15px;
+    text-align: justify;
+    margin-bottom: 130px;
+    margin-top:20px;
+
+`
+
+
+
 export const InputSearchData = styled.input`
+    max-width: 300px;
+    width: 80%;
+    height: 25px;
+    margin: 30px 0;
+    position: fixed;
+        top: 30px;
 `;
 
 export const Span = styled.span`
 font-weight:bold;
 `;
+
+export const Calculate = styled.button`
+font-family: sans-serif;
+cursor:pointer;
+position: fixed;
+bottom: 30px;
+width: 100%;
+  max-width: 296px;
+  height: 48px;
+  margin: 30px 0;
+  border-radius: 2px;
+  border: solid 0;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 22px;
+  transition: background-color 0.2s;
+
+  background: #F9B12A;
+  color: black;
+
+
+
+  &:hover {
+    background: #fcc45d;
+  }
+
+  transition: width 0.7s linear;
+`;
+
+
